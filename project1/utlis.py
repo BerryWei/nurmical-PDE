2269b86a5c6f6dff7e6996025ba6e1a0eb076dc8
BerryWei <berrya90239@gmail.com>
2023-03-06 02:11:25 +0800

    Project 1 Upload!

---
+++ project1/utlis.py
@@ -0,0 +1,110 @@
+import numpy as np
+from scipy.optimize import root
+import sys 
+
+class ODESolver:
+    def __init__(self, rhs, dt, t_span, y0, solver='hybird', jac = None) -> None:
+        self.rhs = rhs # rhs of ODE system
+        self.solver = solver
+        self.dt = dt # time step
+        self.t_span = t_span # time span
+        self.y0 = y0  # initial condition
+        self.jac = jac  # Jacobian function
+        # ---- 
+        self.n_steps = int((t_span[1] - t_span[0]) / dt)
+        self.sol = np.zeros((self.n_steps + 1, len(y0)))
+        self.sol[0] = y0
+
+        # dictionary of available solvers
+        self.dir_solvers = {
+            'hybird': root,
+            'newton': self.newton_raphson,
+            'moore_penrose': self.moore_penrose
+        }
+
+    def solve(self, **kwargs):
+        solver_fn = self.dir_solvers[self.solver]
+        for i in range(self.n_steps):
+            y_prev = self.sol[i]
+            t_prev = i * self.dt
+            t_next = (i + 1) * self.dt
+            # find root
+            if self.solver == 'newton' or self.solver == 'moore_penrose':
+                res = solver_fn(self.rhs, y_prev)
+            else:
+
+                res = solver_fn(lambda x: x - y_prev - self.dt * self.rhs(t_next, x),
+                                y_prev, method='hybr', tol=1e-6, jac=self.jac,
+                                options={'maxfev': 100})
+            self.sol[i + 1] = res.x
+
+    @staticmethod
+    def newton_raphson(fun : callable, x0 : np.ndarray, eps=1e-6, max_iter=100):
+        r'''Find a root of a vector function.
+        Parameters
+        ----------
+        fun: callable
+        x0: initial guess
+        '''
+        n = len(x0)
+        x = x0.copy()
+
+        for i in range(max_iter):
+            # compute Jacobian
+            J = ODESolver.jacobian(fun, x)
+            print(f"det(J)= {np.linalg.det(J)}")
+            print(J)
+            # update x
+            #dx = - np.linalg.inv(J) @ fun(x) --> slower
+            dx = np.linalg.solve(J, -fun(x))
+            x = x + dx
+
+            # check if convergence
+            if np.linalg.norm(dx) < eps:
+                return x
+        return RuntimeError(f"newton_raphson failed to converge after {max_iter} iterations.")
+    
+
+    def moore_penrose(fun, x0, eps=1e-6, max_iter=100):
+        n = len(x0)
+        x = x0.copy()
+
+        for i in range(max_iter):
+            # compute Jacobian
+            J = ODESolver.jacobian(fun, x)
+            print(J.T @ J)
+            # check if Jacobian is singular
+            if np.linalg.cond(J) > 1 / sys.float_info.epsilon:
+                # compute the pseudo inverse of J
+                J_pinv = np.linalg.inv(J.T @ J) @ J.T
+                dx = J_pinv @ -fun(x)
+            else:
+                dx = np.linalg.solve(J, -fun(x))
+
+            x = x + dx
+
+            # check if convergence
+            if np.linalg.norm(dx) < eps:
+                return x
+
+        raise RuntimeError(f"newton_raphson failed to converge after {max_iter} iterations.")
+    
+    @staticmethod
+    def jacobian(fun : callable, x : np.ndarray, h=1e-6):
+        n = len(x)
+        J = np.zeros((n, n))
+        x0 = x.copy()
+        for i in range(n):
+            
+            # calculate partial derivatives
+            x0[i] += h
+            f1 = fun(x0)
+            x0[i] -= 2 * h
+            f2 = fun(x0)
+            x0[i] += h
+            df = (f1 - f2) / (2 * h)
+
+            # fill in Jacobian matrix
+            J[:, i] = df
+        return J
+
